5bad8894df4abe199f7790f12cd40a97e063eb48
phetanshi <padmasekhar.pottepalem@outlook.com>
2023-08-21 19:02:10 +0530

    Chagned Fail message

--- SkillServices.Api/Constants/AppConstants.cs
+++ SkillServices.Api/Constants/AppConstants.cs
@@ -5,7 +5,7 @@
         public const string AuthenticationType = "JwtServerAuth";
         public const int ExpiryTimeInMinutes = 60;
         public const string SUCCESS = "Success!";
-        public const string FAIL = "Something went wrong. Please try again after sometime!";
+        public const string FAIL = "Something went wrong. Please try again after sometime or contact admin!";
         public const string TOKEN_EXPIRED = "Authentication token expired!";
         public const string TOKEN_NOT_EXPIRED = "Authentication token is still alive!";
 
