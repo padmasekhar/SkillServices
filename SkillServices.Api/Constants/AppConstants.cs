d06d2d8ebed435f5b219db3b1c6c9f57c41f221d
phetanshi <padmasekhar.pottepalem@outlook.com>
2023-08-21 17:49:59 +0530

    Success message chagned

--- SkillServices.Api/Constants/AppConstants.cs
+++ SkillServices.Api/Constants/AppConstants.cs
@@ -4,7 +4,7 @@
     {
         public const string AuthenticationType = "JwtServerAuth";
         public const int ExpiryTimeInMinutes = 60;
-        public const string SUCCESS = "Success!";
+        public const string SUCCESS = "Request was successfull!";
         public const string FAIL = "Something went wrong. Please try again after sometime!";
         public const string TOKEN_EXPIRED = "Authentication token expired!";
         public const string TOKEN_NOT_EXPIRED = "Authentication token is still alive!";
